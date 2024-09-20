63053fe28729739ea4db720186fc9905396d5e58
Sandeep Bandaru <sandeepbandaru@google.com>
2024-09-20 14:55:25 +0000

    Add gpu_device access to isolated_compute_app
    Currently all apps except `isolated_app_all` already have this access. Since `isolated_compute_app` is a special case of `isolated_app` with relaxed security restrictions to allow hardware acceleration use cases - it is safe to extend the same access as rest of the apps(system/sepolicy/private/app.te;l=389-394), to `isolated_compute_app`.
    
    Test: tested with latest build, gpu inference works without any permission denials.
    Bug: 368056520
    Change-Id: I5b512cff0d2e68efc03fdc626970b29546ed9f7b
    (cherry picked from commit 702797d6e8df725258a20b6fcbbf3e94f1e89ce7)

--- tests/sepolicy_tests.py
+++ tests/sepolicy_tests.py
@@ -182,6 +182,7 @@ def TestIsolatedAttributeConsistency(test_policy):
         # access given from technical_debt.cil
         "codec2_config_prop" : ["file"],
         "device_config_nnapi_native_prop":["file"],
+        "gpu_device": ["dir"],
         "hal_allocator_default":["binder", "fd"],
         "hal_codec2": ["binder", "fd"],
         "hal_codec2_hwservice":["hwservice_manager"],
@@ -206,6 +207,7 @@ def TestIsolatedAttributeConsistency(test_policy):
         "media_variant_prop":["file"],
         "nnapi_ext_deny_product_prop":["file"],
         "servicemanager":["fd"],
+        "sysfs_gpu": ["file"],
         "toolbox_exec": ["file"],
         # extra types being granted to isolated_compute_app
         "isolated_compute_allowed":["service_manager", "chr_file"],
